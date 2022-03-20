21a326aa3615d286aef536333eb2f497fb98fd64
Akash Gher <53932047+Akash-Gher@users.noreply.github.com>
2022-03-20 18:32:38 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+        React  APP Host test
+        </h1>
+        <a
+         
+        >
+    
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
